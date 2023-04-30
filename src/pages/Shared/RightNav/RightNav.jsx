68f5853ddb7efd463e4f02fcdabc36da52e4251d
numanrahi <arahi2994@gmail.com>
2023-05-01 00:31:23 +0600

    news section added

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -7,11 +7,11 @@ import bg from '../../../assets/bg.png'
 const RightNav = () => {
     return (
         <div>
-            <h4>Login With</h4>
+            <h4 className='mt-4'>Login With</h4>
             <Button className='mb-2 w-100' variant="outline-primary"><FaGoogle /> Login With Google</Button>
             <Button className='w-100' variant="outline-secondary"><FaGithub></FaGithub> Login With Github</Button>
             <div>
-                <h4>Find us on</h4>
+                <h4 className='mt-4'>Find us on</h4>
                 <ListGroup>
                     <ListGroup.Item className='text-center'><FaFacebook className='me-2'></FaFacebook>Facebook</ListGroup.Item>
                     <ListGroup.Item className='text-center'><FaTwitter className='me-2'></FaTwitter>Twitter</ListGroup.Item>
