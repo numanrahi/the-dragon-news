473285049fcd4307663d77163948bd267e50458b
numanrahi <arahi2994@gmail.com>
2023-05-01 20:06:38 +0600

    category added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -12,11 +12,13 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element: <Home></Home>
+                element: <Category></Category>,
+                loader: ()=> fetch('http://localhost:5000/news')
             },
             {
                 path: '/category/:id',
-                element: <Category></Category>
+                element: <Category></Category>,
+                loader: ({params})=> fetch(`http://localhost:5000/categories/${params.id}`)
             }
         ]
     },
@@ -26,7 +28,8 @@ const router = createBrowserRouter([
         children: [
             {
                 path: ':id',
-                element: <News></News>
+                element: <News></News>,
+                loader: ({params})=> fetch(`http://localhost:5000/news/${params.id}`)
             }
         ]
     }
